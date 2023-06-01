252963370d2da89212588f10ce04978d01e0fa7e
Omar Elshaer <78622723+Omarelshaer2@users.noreply.github.com>
2023-06-02 01:44:17 +0300

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - checks if a binary tree is perfect
- * @tree: a pointer to the root node of the tree to check
+ * binary_tree_is_perfect - this checks if the binary tree that we want is perfect or what
+ * @tree: a pointer to root of the tree we want to check
  *
  * Return: 1 if the tree is perfect
  *         0 if the tree is not perfect
