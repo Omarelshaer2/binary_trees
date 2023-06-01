58b433c5f52589c8b7961166f869188245604d97
Omar Elshaer <78622723+Omarelshaer2@users.noreply.github.com>
2023-06-02 01:52:10 +0300

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle - finds the uncle of a node
- * @node: a pointer to the node to find the uncle
+ * binary_tree_uncle - this also finds uncle of the root of the node
+ * @node: a pointer to the root to find us the uncle that we want
  *
  * Return: pointer to the uncle node
  *         NULL if node is NULL
@@ -18,10 +18,10 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 }
 
 /**
- * binary_tree_sibling - finds the sibling of a node
- * @node: pointer to the node to find the sibling
+ * binary_tree_sibling - also to finds the sibling of the root nod we search for
+ * @node: pointer to the root node to find the missing one
  *
- * Return: pointer to the sibling node
+ * Return: pointer to the missing node we want to
  *         NULL if node is NULL
  *         NULL if the parent is NULL
  *         NULL if the node has no siblings
