b31310349999bd4ffc638ceaf4fff297487db0ca
Omar Elshaer <78622723+Omarelshaer2@users.noreply.github.com>
2023-06-02 01:23:11 +0300

    Update 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - goes through a binary tree using in-order traversal
- * @tree: tree to traverse
- * @func: pointer to a function to call for each node
+ * binary_tree_inorder - this is going through a binary tree by use in-order traveler
+ * @tree: this is just a tree to traverse
+ * @func: a just pointer to function that we want to call for each node
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
